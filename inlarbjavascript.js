fac34ee82699c9bfdaaef23d10eeeeaad3ed7b51
Anton Kulchytskyi <anton@oykos-hub.com>
2020-06-06 02:07:55 +0200

    Add files via upload

---
+++ inlarbjavascript.js
@@ -0,0 +1,39 @@
+// Open & close user account settings
+function openNav() {
+    document.getElementById("mySidenav").style.width = "20em";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+// Slide show with top events
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+// Go to top button
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
